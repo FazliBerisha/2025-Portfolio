a92072af018a633cd1915fa265a6e5dafefd9381
FazliBerisha <fazliberisha03@gmail.com>
2024-10-03 15:32:26 -0400

    Implemented new projects

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,6 +1,28 @@
 export const projectsData = [
     {
         id: 1,
+        name: "AniBot: AI-Powered Anime Recommendation Chatbot",
+        description: "Developing an AI-powered chatbot to recommend anime movies and TV shows based on user preferences such as genre, timeline, mood, and other factors. The project aims to help anime enthusiasts discover new content tailored to their tastes. Features include personalized recommendations, genre-based filtering, mood-based suggestions, timeline-specific recommendations, user preference learning, and a conversational interface.",
+        tools: ["Python", "FastAPI", "MongoDB", "TensorFlow", "spaCy", "React.js"],
+        role: "Full Stack Developer",
+        code: "",
+        demo: "",
+        image: null,
+        date: "Ongoing"
+    },
+    {
+        id: 2,
+        name: "Community Tool Lending Platform",
+        description: "Creating a community-driven platform for lending and borrowing tools. The project consists of a React frontend application and a FastAPI backend application.",
+        tools: ["React.js", "FastAPI"],
+        role: "Full Stack Developer",
+        code: "",
+        demo: "",
+        image: null,
+        date: "Ongoing"
+    },
+    {
+        id: 3,
         name: 'Dental Symptom Checker',
         description: "Engineered a full-stack web application for dental health assessment using real-time data from the OpenFDA API. Designed a responsive interface with React, optimizing performance and reducing load times by 40%. Developed a robust backend with Node.js and Express, ensuring data consistency and security. Integrated third-party services for additional functionalities and optimized database queries to enhance system performance and reliability.",
         tools: ['React', 'Node.js', 'Express', 'OpenFDA API'],
@@ -11,7 +33,7 @@ export const projectsData = [
         date: 'Aug. 2024'
     },
     {
-        id: 2,
+        id: 4,
         name: 'Weather Dashboard',
         description: 'Developed a full-stack weather application providing real-time weather data and forecasts, integrating the OpenWeatherMap API. Designed a responsive React frontend with interactive charts (Recharts) and dynamic maps (Leaflet). Implemented a robust Flask backend for efficient API request handling and data processing.',
         tools: ['React', 'Flask', 'Python', 'OpenWeatherMap API', 'Recharts', 'Leaflet'],
@@ -22,7 +44,7 @@ export const projectsData = [
         date: 'Aug. 2024'
     },
     {
-        id: 3,
+        id: 5,
         name: 'Blog Management Website',
         description: 'Developed a secure blog platform with CRUD functionalities for creating, reading, updating, and deleting blog posts. Utilized phpMyAdmin for database management and implemented secure password storage. Enhanced site performance with faster load times and ensured cross-browser compatibility, achieving a 100% reduction in XSS vulnerabilities.',
         tools: ['PHP', 'MySQL', 'HTML', 'CSS', 'TinyMCE'],
@@ -33,7 +55,7 @@ export const projectsData = [
         date: 'Jan. 2024 – May 2024'
     },
     {
-        id: 4,
+        id: 6,
         name: 'Matrix Operation Program',
         description: 'Designed and implemented a C program in a Linux environment that generated and processed matrices of up to 10x10 dimensions, performing operations such as addition, subtraction, multiplication, and division. Optimized memory management by dynamically allocating and deallocating space for 2D matrices using custom functions, achieving a 20% reduction in memory usage. Developed a robust error-correction system that successfully identified and corrected up to 95% of introduced errors in matrix data, ensuring high accuracy and reliability. Part of a Coastal Carolina University coursework project.',
         tools: ['C', 'Linux'],
